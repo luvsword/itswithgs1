f5291310ff1a70e1970c8d0e749269e8ff11b85e
showmethejinw <king403@kaist.ac.kr>
2016-11-18 22:32:29 +0900

    Update AppConfig.java

--- app/src/main/java/com/example/jang/maptest/App/AppConfig.java
+++ app/src/main/java/com/example/jang/maptest/App/AppConfig.java
@@ -7,6 +7,6 @@ package com.example.jang.maptest.App;
 public class AppConfig {
     // EPCIS Server
 
-    public static final String EPCIS_SERVER_CAPTURE =  "http://143.248.56.100:8080/epcis/#";
-    public static final String EPCIS_SERVER_QUERY = "http://143.248.56.100:8080/epcis/#";
+    public static final String EPCIS_SERVER_CAPTURE =  "";
+    public static final String EPCIS_SERVER_QUERY = "";
 }
